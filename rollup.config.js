92f00a1282f1355daf156de5d8ee814bf408deec
Morante <smorantecano@gmail.com>
2020-04-09 23:00:30 +0200

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,29 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
-import { cpy } from 'rollup-plugin-cpy';
+import cpy from 'rollup-plugin-cpy';
+import { createCompatibilityConfig } from '@open-wc/building-rollup';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
+const config = createCompatibilityConfig({
+  input: './index.html',
+});
 
-const config = createDefaultConfig({ input: './index.html' });
-
+// if you use an array of configs, you don't need the copy task to be executed for both builds.
+// we can add the plugin only to the first rollup config:
 export default [
+  // add plugin to the first config
   {
     ...config[0],
     plugins: [
       ...config[0].plugins,
       cpy({
-        files: ['manifest.json', 'assets/**/*'],
+        files: ['manifest.json','assets/**/*'],
         dest: 'dist',
         options: {
-          parent: true,
+          // parents makes sure to preserve the original folder structure
+          parents: true,
         },
       }),
     ],
   },
 
+  // leave the second config untouched
   config[1],
 ];
