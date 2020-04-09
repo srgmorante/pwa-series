5406388a2a91aaf6d0cd49456b79cdf52376f508
Morante <smorantecano@gmail.com>
2020-04-09 22:40:38 +0200

    manifest and sw  pwa support

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,26 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
+import { cpy } from 'rollup-plugin-cpy';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html' });
+const config = createDefaultConfig({ input: './index.html' });
+
+export default [
+  {
+    ...config[0],
+    plugins: [
+      ...config[0].plugins,
+      cpy({
+        files: ['manifest.json', 'assets/**/*'],
+        dest: 'dist',
+        options: {
+          parent: true,
+        },
+      }),
+    ],
+  },
+
+  config[1],
+];
